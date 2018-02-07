e905e0907306452c613806fda488f4ce3cce575f
userVadim <vadim.fedenev@gmail.com>
2018-02-07 12:00:19 +0200

    Update index.php
    getting new paths to images

--- views/blog/index.php
+++ views/blog/index.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\StringHelper;
 use yii\helpers\Html;
+use app\models\ImageUploader;
 ?>
 
 <div class="home-div">
@@ -11,7 +12,7 @@ use yii\helpers\Html;
     <div class="one-item-home">
         <h3><?= Html::a($item->title, ['blog/view-item','id'=>$item->id])?></h3>
         <p><?= StringHelper::truncate($item->content, 250)?></p>
-        <p><?=Html::img('@web/uploads/userphotos/'.$item->user->photo,['height'=>'30px'])?><?=$item->user->username?></p>
+        <p><?=Html::img(ImageUploader::pathToUserPhoto().$item->user->photo,['height'=>'30px'])?><?=$item->user->username?></p>
     </div>
         <?php
     }
