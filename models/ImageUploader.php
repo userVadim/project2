146c673d170b07052ba59019a9c7fc1bf2b09d52
userVadim <vadim.fedenev@gmail.com>
2018-02-07 11:55:08 +0200

    Updating ImageUploader
    - new const(PATH_PREFIX) and 2 functions(pathToUserPhoto, pathToContent) to get paths in views 
    - new function(setImageNameAndSave) for images saving into needed folders optimization. Returns image name

--- models/ImageUploader.php
+++ models/ImageUploader.php
@@ -11,25 +11,33 @@ use yii\helpers\FileHelper;
 class ImageUploader extends Model {
     const USER_PHOTO_DIR='uploads/userphotos/';
     const ITEM_CONTENT_DIR='uploads/content/';
+    const PATH_PREFIX='@web/';
     
-    public $username;
     public $user_photo;
     public $content_images;
     
     public function rules() {
         return [
             [['content_images'],'file', 'extensions'=>['jpg','png'], 'checkExtensionByMimeType'=>false, 'maxFiles'=>10],
-            ['user_photo', 'file', 'extensions'=>'jpg, png', 'checkExtensionByMimeType'=>false],
-            
+            ['user_photo', 'file', 'extensions'=>'jpg, png', 'checkExtensionByMimeType'=>false],   
         ];
+    }
+    
+    public function pathToUserPhoto()
+    {
+        return self::PATH_PREFIX.self::USER_PHOTO_DIR;
+    }
+    
+    public function pathToContent()
+    {
+        return self::PATH_PREFIX.self::ITEM_CONTENT_DIR;
     }
     
     public function uploadUserPhoto()
     {
         if($this->user_photo && $this->validate())
         {
-            $this->user_photo->saveAs(self::USER_PHOTO_DIR.$this->username.time().".".$this->user_photo->extension);
-            return $this->username.time().".".$this->user_photo->extension;
+            return $this->setImageNameAndSave(self::USER_PHOTO_DIR, $this->user_photo);
         }
         return false;
     }
@@ -40,16 +48,23 @@ class ImageUploader extends Model {
         {
             foreach($this->content_images as $image)
             {
-                do{
-                    $name=Yii::$app->getSecurity()->generateRandomString(12);
-                    $fullPath=self::ITEM_CONTENT_DIR.$name.".".$image->extension;
-                }
-                while (file_exists($fullPath));
-                $image->saveAs($fullPath);
-                $bulk[]=$name.".".$image->extension;
+                $bulk[]=$this->setImageNameAndSave(self::ITEM_CONTENT_DIR, $image);
             }
             return Json::encode($bulk);
         }
-
+        return false;
+    }
+    
+    private function setImageNameAndSave($folder,$image)
+    {
+        do{
+            $name=Yii::$app->getSecurity()->generateRandomString(12);
+            $fullPath=$folder.$name.".".$image->extension;
+        }
+        while (file_exists($fullPath));
+        if($image->saveAs($fullPath))
+        {
+            return $name.".".$image->extension;
+        }
     }
 }
